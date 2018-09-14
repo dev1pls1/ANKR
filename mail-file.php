e7d135cbc4c927677a287973bd1b30cdf23c3be2
fruit37 <stanleywu84@gmail.com>
2018-09-13 22:55:16 -0700

    adding the mail service

--- mail-file.php
+++ mail-file.php
@@ -1,66 +1,89 @@
 <?php
-  
+  require 'vendor/autoload.php';
+
+  use Aws\Ses\SesClient;
+  use Aws\Exception\AwsException;
 
   $file = false;
+  $sender_email = 'no-reply@ankr.network';
+  $to_email = 'career@ankr.network'
 
- 
   if ($_FILES['file']['size']) {
     $tmp_file = $_FILES["file"]["tmp_name"];
-    $file = './' . basename($_FILES['file']['name']);
-    move_uploaded_file($tmp_file, $file);
+    $file = $_FILES['file']['name'];
+    // move_uploaded_file($tmp_file, $file);
   }
 
-  $to = "career@ankr.network";
+  $recipient_emails = [$to_email];
+  $subject = "Ankr Network Uploaded file";
 
-  $sitename = "ANKR-Network";
-  $subject = "$sitename Uploaded file";
+  $SesClient = new SesClient([
+    'profile' => 'default',
+    'version' => '2010-12-01',
+    'region'  => 'us-west-2'
+  ]);
 
-  $message = '
-    <html>
-      <head>
-        <title>'.$subject.'</title>
-      </head>
-      <body>';
+  $html_body = '
+<html>
+  <head>
+    <title>'.$subject.'</title>
+  </head>
+  <body>';
         if($file){
-          $message .= '<p>File: '.basename($_FILES['file']['name']).'</p> ';
+          $html_body .= '<p>File: '.basename($file).'</p> ';
         }
-  $message .=                              
-      '</body>
-    </html>';
+  $html_body .=
+  '</body>
+</html>';
+  $char_set = 'UTF-8';
 
-  $result = sendMailAttachment($to, $subject, $message, $file);
-  if($file){
-    unlink($file);
+  try {
+    $separator = md5(time());
+
+    $headers = "From: $sender_email\n";
+    $headers .= "To: $to_email\nReply-To: $sender_email\n";
+    $headers .= "Subject: $subject\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-Type: multipart/mixed; boundary=\"$separator\"\n";
+
+    $bodyMail = "--$separator\n";
+    $bodyMail .= "Content-Type: text/html; charset=\"$char_set\"\n";
+    $bodyMail .= "Content-Transfer-Encoding: 8bit\n";
+    $bodyMail .= $html_body."\n";
+
+    if($file){
+      $bodyMail .= "--$separator\n";
+      $fileRead = fopen($tmp_file, "r");
+      $contentFile = fread($fileRead, filesize($tmp_file));
+      fclose($fileRead);
+      $bodyMail .= "Content-Type: application/octet-stream; name=\"=?utf-8?B?".base64_encode(basename($file))."?=\"\n";
+      $bodyMail .= "Content-Transfer-Encoding: base64\n";
+      $bodyMail .= "Content-Disposition: attachment; filename=\"=?utf-8?B?".base64_encode(basename($file))."?=\"\n\n";
+      $bodyMail .= chunk_split(base64_encode($contentFile))."\n";
+      $bodyMail .= "--".$separator ."--\n";
+      $result = $SesClient->sendRawEmail([
+          'Destination' => [
+              'ToAddresses' => $recipient_emails,
+          ],
+          'ReplyToAddresses' => [$sender_email],
+          'Source' => $sender_email,
+          'RawMessage' => [
+              'Data' => "$headers\n$bodyMail",
+          ]
+      ]);
+      $messageId = $result['MessageId'];
+    }
+
+    // echo("Email sent! Message ID: $messageId"."\n");
+    echo("SUCCESS. Email has been sent!");
+  } catch (AwsException $e) {
+    // output error message if fails
+    // echo $e->getMessage();
+    // echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+    echo "ERROR";
   }
-  
 
-echo $result ? 'SUCCESS. Email has been sent!' : 'ERROR';
-
-function sendMailAttachment($mailTo, $subject, $message, $file = false){
-  $separator = "---";
-
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers .= "From: $from\nReply-To: $from\n";
-  $headers .= "Content-Type: multipart/mixed; boundary=\"$separator\"";
-
-  $bodyMail = "--$separator\n";
-  $bodyMail .= "Content-Type:text/html; charset=\"utf-8\"\n";
-  $bodyMail .= "Content-Transfer-Encoding: 7bit\n\n";
-  $bodyMail .= $message."\n";
   if($file){
-    $bodyMail .= "--$separator\n";
-    $fileRead = fopen($file, "r");
-    $contentFile = fread($fileRead, filesize($file));
-    fclose($fileRead);
-    $bodyMail .= "Content-Type: application/octet-stream; name==?utf-8?B?".base64_encode(basename($file))."?=\n"; 
-    $bodyMail .= "Content-Transfer-Encoding: base64\n";
-    $bodyMail .= "Content-Disposition: attachment; filename==?utf-8?B?".base64_encode(basename($file))."?=\n\n";
-    $bodyMail .= chunk_split(base64_encode($contentFile))."\n";
-    $bodyMail .= "--".$separator ."--\n";
+    unlink($tmp_file);
   }
-  $result = mail($mailTo, $subject, $bodyMail, $headers);
-  return $result;
-}
-
-
 ?>
--- mail.php
+++ mail.php
@@ -1,42 +1,61 @@
 <?php
+  require 'vendor/autoload.php';
+
+  use Aws\Ses\SesClient;
+  use Aws\Exception\AwsException;
+
+  $sender_email = 'no-reply@ankr.network';
 
   $email = ( isset($_POST["Email"]) ) ? trim($_POST["Email"]) : '';
+  $recipient_emails = ['info@anker.network'];
+  $subject = "Ankr Network Feedback from subscriber $email";
 
-  $to = "info@anker.network";
+  $SesClient = new SesClient([
+    'profile' => 'default',
+    'version' => '2010-12-01',
+    'region'  => 'us-west-2'
+  ]);
 
-  $sitename = "ANKR-Network";
-  $subject = "$sitename Feedback from subscriber $email";
-
-  $message = '
+  $html_body = '
     <html>
       <head>
         <title>'.$subject.'</title>
       </head>
       <body>
-        <p>Email: '.$email.'</p>';
-  $message .=                              
-      '</body>
+        <p>Email: '.$email.'</p>
+      </body>
     </html>';
+  $char_set = 'UTF-8';
 
-  $result = sendMailAttachment($to, $email, $subject, $message);
-  
-
-echo $result ? 'SUCCESS. Email has been sent!' : 'ERROR';
-
-function sendMailAttachment($mailTo, $from, $subject, $message){
-  $separator = "---";
-
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers .= "From: $from\nReply-To: $from\n";
-  $headers .= "Content-Type: multipart/mixed; boundary=\"$separator\"";
-
-  $bodyMail = "--$separator\n";
-  $bodyMail .= "Content-Type:text/html; charset=\"utf-8\"\n";
-  $bodyMail .= "Content-Transfer-Encoding: 7bit\n\n";
-  $bodyMail .= $message."\n";
-  $result = mail($mailTo, $subject, $bodyMail, $headers);
-  return $result;
-}
-
-
+  try {
+    if ($sender_email) {
+      $result = $SesClient->sendEmail([
+          'Destination' => [
+              'ToAddresses' => $recipient_emails,
+          ],
+          'ReplyToAddresses' => [$sender_email],
+          'Source' => $sender_email,
+          'Message' => [
+            'Body' => [
+                'Html' => [
+                    'Charset' => $char_set,
+                    'Data' => $html_body,
+                ]
+            ],
+            'Subject' => [
+                'Charset' => $char_set,
+                'Data' => $subject,
+            ],
+          ]
+      ]);
+      $messageId = $result['MessageId'];
+      // echo("Email sent! Message ID: $messageId"."\n");
+    }
+    echo("SUCCESS. Email has been sent!");
+  } catch (AwsException $e) {
+    // output error message if fails
+    // echo $e->getMessage();
+    // echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+    echo "ERROR";
+  }
 ?>
