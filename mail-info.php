d76328446ece5b3419a434ded815739dd33f1952
Stanley Wu <35579150+fruit37@users.noreply.github.com>
2018-12-13 22:57:51 -0800

    editing mail file

--- mail-info.php
+++ mail-info.php
@@ -1,4 +1,8 @@
 <?php
+  require 'vendor/autoload.php';
+
+  use Aws\Ses\SesClient;
+  use Aws\Exception\AwsException;
 
   $name = ( isset($_POST["Name"]) ) ? trim($_POST["Name"]) : '';
   $email = ( isset($_POST["Email"]) ) ? trim($_POST["Email"]) : '';
@@ -53,31 +57,59 @@
 
 echo $result ? 'SUCCESS. Email has been sent!' : 'ERROR';
 
-function sendMailAttachment($mailTo, $from, $subject, $message, $file = false){
-  $separator = "---";
+function sendMailAttachment($to_email, $reply_email, $subject, $html_body, $file = false){
+  try {
+    $SesClient = new SesClient([
+      'version' => '2010-12-01',
+      'region'  => 'us-west-2'
+    ]);
 
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers .= "From: $from\nReply-To: $from\n";
-  $headers .= "Content-Type: multipart/mixed; boundary=\"$separator\"";
+    $char_set = 'UTF-8';
+    $separator = md5(time());
+    $sender_email = 'no-reply@ankr.network';
 
-  $bodyMail = "--$separator\n";
-  $bodyMail .= "Content-Type:text/html; charset=\"utf-8\"\n";
-  $bodyMail .= "Content-Transfer-Encoding: 7bit\n\n";
-  $bodyMail .= $message."\n";
-  if($file){
-    $bodyMail .= "--$separator\n";
-    $fileRead = fopen($file, "r");
-    $contentFile = fread($fileRead, filesize($file));
-    fclose($fileRead);
-    $bodyMail .= "Content-Type: application/octet-stream; name==?utf-8?B?".base64_encode(basename($file))."?=\n"; 
-    $bodyMail .= "Content-Transfer-Encoding: base64\n";
-    $bodyMail .= "Content-Disposition: attachment; filename==?utf-8?B?".base64_encode(basename($file))."?=\n\n";
-    $bodyMail .= chunk_split(base64_encode($contentFile))."\n";
-    $bodyMail .= "--".$separator ."--\n";
+    $headers = "From: $sender_email\n";
+    $headers .= "To: $to_email\nReply-To: $reply_email\n";
+    $headers .= "Subject: $subject\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-Type: multipart/mixed; boundary=\"$separator\"\n";
+
+    $bodyMail = "--$separator\n";
+    $bodyMail .= "Content-Type: text/html; charset=\"$char_set\"\n";
+    $bodyMail .= "Content-Transfer-Encoding: 8bit\n";
+    $bodyMail .= $html_body."\n";
+
+    if($file){
+      $bodyMail .= "--$separator\n";
+      $fileRead = fopen($tmp_file, "r");
+      $contentFile = fread($fileRead, filesize($tmp_file));
+      fclose($fileRead);
+      $bodyMail .= "Content-Type: application/octet-stream; name=\"=?utf-8?B?".base64_encode(basename($file))."?=\"\n";
+      $bodyMail .= "Content-Transfer-Encoding: base64\n";
+      $bodyMail .= "Content-Disposition: attachment; filename=\"=?utf-8?B?".base64_encode(basename($file))."?=\"\n\n";
+      $bodyMail .= chunk_split(base64_encode($contentFile))."\n";
+      $bodyMail .= "--".$separator ."--\n";
+    }
+
+    $result = $SesClient->sendRawEmail([
+      'Destination' => [
+          'ToAddresses' => [$to_email],
+      ],
+      'ReplyToAddresses' => [$reply_email],
+      'Source' => $sender_email,
+      'RawMessage' => [
+          'Data' => "$headers\n$bodyMail",
+      ]
+    ]);
+    $messageId = $result['MessageId'];
+
+    // echo("Email sent! Message ID: $messageId"."\n");
+    return true;
+  } catch (AwsException $e) {
+    // output error message if fails
+    // echo $e->getMessage();
+    // echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+    return false;
   }
-  $result = mail($mailTo, $subject, $bodyMail, $headers);
-  return $result;
 }
-
-
 ?>
